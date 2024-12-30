0a38b63bff79d6ae02477c2907cf05f7df5bcb74
jitendra4545 <jitendra45.js45@gmail.com>
2024-12-30 12:17:58 +0530

    feat-backend inital setup done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,19 @@
 
+require('dotenv').config();
+const express = require('express');
+const connection = require('./config/db');
+const app = express();
 
-let abcd 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(process.env.PORT, async () => {
+    try {
+        await connection
+        console.log('Database connected');
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
